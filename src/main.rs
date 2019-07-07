0551bcebd96c78ec9f78380079d83055d4da21d0
George <minallkamel@gmail.com>
2019-07-08 01:17:13 +0200

    add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Crates
+extern crate indicatif;
+
+// Use
+use indicatif::ProgressBar;
 use std::fs;
 use std::io;
 use std::path::Path;
@@ -5,7 +10,7 @@ use std::path::PathBuf;
 
 // Main
 fn main() {
-  let path = Path::new("E:\\download\\The Big Bang");
+  let path = Path::new("E:\\download\\Brooklyn-s06");
   let all_dir_paths = dir_list(&path).unwrap();
   talk_a_walk(&path, &all_dir_paths).unwrap();
 }
@@ -28,14 +33,22 @@ fn dir_list(dir: &Path) -> Result<Vec<PathBuf>, io::Error> {
 // Go into each folder and move the files
 fn talk_a_walk(copy_to_path: &Path, all_dir_paths: &Vec<PathBuf>) -> Result<(), io::Error> {
   if all_dir_paths.len() > 0 {
+    // Create a progress bar
+    let pbar_total = all_dir_paths.len() as u64;
+    let pbar = ProgressBar::new(pbar_total);
+    // Walk the directories and copy files
     for dir in all_dir_paths {
+      // Increment for each folder
+      pbar.inc(1);
+      // Entry is a file in dir
       for entry in fs::read_dir(dir)? {
         let entry = entry?.path();
         let name = entry.file_name().unwrap();
         let formatted_path = PathBuf::from(copy_to_path.join(name));
         let new_path = Path::new(&formatted_path);
+        // Check if the file is not already there
         if new_path.is_file() == false {
-          // Copy every old path to main directory
+          // Copy every old path to its new path in main directory
           fs::copy(&entry, &new_path)?;
         } else {
           println!("File already there!: {:?}", name);
@@ -43,8 +56,8 @@ fn talk_a_walk(copy_to_path: &Path, all_dir_paths: &Vec<PathBuf>) -> Result<(), 
       }
     }
     println!("Done!");
-    return Ok(());
   } else {
-    return Err(io::Error::new(io::ErrorKind::Other, "No directories!"));
+    println!("No directories!");
   }
+  Ok(())
 }
